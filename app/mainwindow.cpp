20e05a3572575bbf9dcb6f47c2474677f847c657
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-26 21:36:36 -0400

    mainwindow.cpp: Put braces on the same line.
    This is the preferred coding style for KDE. There's "old" code present
    in yakuake that adheres to "braces on separate lines", but new code
    should use this style.

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1064,16 +1064,13 @@ bool MainWindow::event(QEvent* event)
 void MainWindow::toggleWindowState()
 {
     bool visible = isVisible();
-    if (visible)
-    {
-        if (m_prevFocusWidget)
-        {
+    if (visible) {
+        if (m_prevFocusWidget) {
             // Reset the previously-focused widget if it was set before.
             disconnect(m_prevFocusWidget, &QWidget::destroyed, this, &MainWindow::prevFocusWidgetDestroyed);
         }
         m_prevFocusWidget = this->focusWidget();
-        if (m_prevFocusWidget)
-        {
+        if (m_prevFocusWidget) {
             connect(m_prevFocusWidget, &QWidget::destroyed, this, &MainWindow::prevFocusWidgetDestroyed);
         }
     }
@@ -1149,8 +1146,7 @@ void MainWindow::toggleWindowState()
         }
     }
 
-    if (!visible && m_prevFocusWidget)
-    {
+    if (!visible && m_prevFocusWidget) {
         // Reset the widget focus.
         m_prevFocusWidget->setFocus();
         disconnect(m_prevFocusWidget, &QWidget::destroyed, this, &MainWindow::prevFocusWidgetDestroyed);
